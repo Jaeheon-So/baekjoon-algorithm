b2d114d7a9f4b060690aec162825fccb894784b4
Jaeheon-So <sjh-98@naver.com>
2022-01-28 20:56:08 +0900

    전깃줄**

--- 동적계획법/2565.py
+++ 동적계획법/2565.py
@@ -1,2 +1,14 @@
 #전깃줄
-#try again
+location = []
+n = int(input())
+dp = [1]*n
+for _ in range(n):
+    a, b = map(int, input().split())
+    location.append([a, b])
+location = sorted(location, key=lambda a: a[0])
+dp[0] = 1
+for i in range(n):
+    for j in range(i):   
+        if location[j][1] < location[i][1]:
+            dp[i] = max(dp[i], dp[j]+1)
+print(n-max(dp))
