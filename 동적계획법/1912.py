ed09b38713380ec725fbb7312bf969f84a342105
Jaeheon-So <sjh-98@naver.com>
2022-01-30 21:50:04 +0900

    연속합

--- 동적계획법/1912.py
+++ 동적계획법/1912.py
@@ -1,2 +1,8 @@
 #연속합
-#Try again
+n = int(input())
+seq = list(map(int, input().split()))
+dp = [0]*n
+dp[0] = seq[0]
+for i in range(1, n):
+    dp[i] = max(seq[i], dp[i-1] + seq[i])
+print(max(dp))
