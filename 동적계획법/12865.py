29e4fe082206a34a698b2af64a55c6d07c093ea9
Jaeheon-So <sjh-98@naver.com>
2022-01-30 22:46:07 +0900

    ***다시 시도***

--- 동적계획법/12865.py
+++ 동적계획법/12865.py
@@ -1,2 +1,21 @@
 #평범한 배낭
-#try again
+things = []
+n, k = map(int, input().split())
+dp = [0]*n
+for _ in range(n):
+    w, v = map(int, input().split())
+    things.append([w, v])
+
+print(things)
+
+# dp[0] = things[0][1]
+# temp = things[0][0]
+for i in range(n):
+    temp = things[i][0]
+    for j in range(i+1, n):
+        if temp+things[j][0] < k:
+            temp += things[j][0]
+            dp[i] = dp[i-1] + things[j][1]
+        else:
+            dp[i] = max(dp[i], things[j][1])
+print(dp)
