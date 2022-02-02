40820ada7043dde92209ef55258e4ff2daecb676
Jaeheon-So <sjh-98@naver.com>
2022-02-02 20:20:35 +0900

    설탕 배달

--- 그리디/2839.py
+++ 그리디/2839.py
@@ -1,13 +1,12 @@
-#설탕 배달***
+#설탕 배달
+n = int(input())
 result = 0
-N = int(input())
-
-while N >= 0:
-    if N % 5 == 0:
-        result += N // 5
+while n >= 0:
+    if n % 5 == 0:
+        result += n // 5
         break
-    N -= 3
-    result += 1    
+    n -= 3
+    result += 1
 else:
     result = -1
 print(result)
