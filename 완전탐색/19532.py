db4d406ae92328195c2a246da89d6e8323109aee
Jaeheon-So <sjh-98@naver.com>
2022-02-12 17:40:49 +0900

    수학은 비대면강의입니다

--- 완전탐색/19532.py
+++ 완전탐색/19532.py
@@ -1,7 +1,7 @@
-#수학은 비대면강의입니다***
-a,b,c,d,e,f = map(int, input().split())
-for i in range(-999, 1000):
-    for j in range(-999, 1000):
-        if a*i + b*j == c and d*i + e*j == f:
-            print(i,j)
+#수학은 비대면강의입니다
+a,b,c,d,e,f, = map(int, input().split())
+for x in range(-999, 1000):
+    for y in range(-999, 1000):
+        if a*x + b*y == c and d*x + e*y == f:
+            print(x, y)
             break
