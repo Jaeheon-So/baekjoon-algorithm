0a1d7e90145d3302261fb373a06a00ce91d81b76
Jaeheon-So <sjh-98@naver.com>
2022-02-13 20:31:49 +0900

    단지번호붙이기

--- DFS, BFS/2667.py
+++ DFS, BFS/2667.py
@@ -28,3 +28,37 @@ result.sort()
 print(len(result))
 for i in result:
     print(i)
+
+# bfs 풀이
+# from collections import deque
+# def bfs(x,y):
+#     queue = deque()
+#     graph[x][y] = 0
+#     cnt = 1
+#     queue.append((x,y))
+#     while queue:
+#         x, y = queue.popleft()
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             if 0<=nx<n and 0<=ny<n and graph[nx][ny] == 1:
+#                 queue.append((nx,ny))
+#                 graph[nx][ny] = 0
+#                 cnt += 1
+#     return cnt
+
+# graph = []
+# dx = [-1,1,0,0]
+# dy = [0,0,-1,1]
+# result = []
+# n = int(input())
+# for _ in range(n):
+#     graph.append(list(map(int, input())))
+# for i in range(n):
+#     for j in range(n):
+#         if graph[i][j] == 1:
+#             result.append(bfs(i, j))
+# result.sort()
+# print(len(result))
+# for i in result:
+#     print(i)
