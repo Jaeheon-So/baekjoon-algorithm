3cf87e210870d6c27d4fbcb520fe66ac52afeb65
Jaeheon-So <sjh-98@naver.com>
2022-02-13 21:04:33 +0900

    유기농 배추

--- DFS, BFS/1012.py
+++ DFS, BFS/1012.py
@@ -5,31 +5,29 @@ def bfs(x,y):
     queue.append((x,y))
     graph[x][y] = 0
     while queue:
-        p, q = queue.popleft()
+        x, y = queue.popleft()
         for i in range(4):
-            nx = p+dx[i]
-            ny = q+dy[i]
-            if nx < 0 or nx >=n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 1:
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny] == 1:
+                queue.append((nx,ny))
                 graph[nx][ny] = 0
-                queue.append((nx, ny))
     return
 
 graph = []
-dx = [-1,1,0,0]
+dx = [-1,1, 0, 0]
 dy = [0,0,-1,1]
 t = int(input())
 for _ in range(t):
-    result = 0
+    cnt = 0
     m, n, k = map(int, input().split())
-    graph = [[0 for j in range(m)] for i in range(n)]
+    graph = [[0 for _ in  range(m)] for _ in range(n)]
     for _ in range(k):
-        x, y = map(int, input().split())
+        x, y =  map(int, input().split())
         graph[y][x] = 1
     for i in range(n):
         for j in range(m):
             if graph[i][j] == 1:
                 bfs(i,j)
-                result += 1
-    print(result)
+                cnt += 1
+    print(cnt)
