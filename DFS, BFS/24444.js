0ca9d75c905cfd98d74be45e031d0aa6fe94d81e
Jaeheon-So <sjh-98@naver.com>
2022-11-30 20:14:02 +0900

    너비 우선 탐색 1

---
+++ DFS, BFS/24444.js
@@ -0,0 +1,37 @@
+// 알고리즘 수업 - 너비 우선 탐색 1
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let [n, m, root] = input[0].split(" ").map((v) => Number(v));
+let graph = Array.from(Array(n + 1), () => Array());
+let visited = Array(n + 1).fill(0);
+let cnt = 1;
+
+for (let i = 1; i < input.length; i++) {
+  let [a, b] = input[i].split(" ").map((v) => Number(v));
+  graph[a].push(b);
+  graph[b].push(a);
+}
+graph = graph.map((v) => v.sort((a, b) => a - b));
+
+bfs(root);
+visited.shift();
+console.log(visited.join("\n"));
+
+function bfs(v) {
+  let q = [];
+  q.push(v);
+  visited[v] = cnt++;
+  while (q.length) {
+    let node = q.shift();
+    for (let nv of graph[node]) {
+      if (!visited[nv]) {
+        visited[nv] = cnt++;
+        q.push(nv);
+      }
+    }
+  }
+}
