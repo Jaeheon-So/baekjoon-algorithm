97cc5cb057df62cdc9a152689fbec9182e50b7c9
Jaeheon-So <sjh-98@naver.com>
2022-11-30 19:57:06 +0900

    깊이 우선 탐색 2

---
+++ DFS, BFS/24480.js
@@ -0,0 +1,32 @@
+// 알고리즘 수업 - 깊이 우선 탐색 2
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let [n, m, root] = input[0].split(" ").map((v) => Number(v));
+let graph = Array.from(Array(n + 1), () => Array());
+let visited = Array(n + 1).fill(0);
+let cnt = 1;
+
+for (let i = 1; i < input.length; i++) {
+  let [a, b] = input[i].split(" ").map((v) => Number(v));
+  graph[a].push(b);
+  graph[b].push(a);
+}
+graph = graph.map((v) => v.sort((a, b) => b - a));
+
+dfs(root);
+visited.shift();
+console.log(visited.join("\n"));
+
+function dfs(node) {
+  visited[node] = cnt;
+  for (let nv of graph[node]) {
+    if (!visited[nv]) {
+      cnt++;
+      dfs(nv);
+    }
+  }
+}
